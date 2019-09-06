27b37724e4de502d830ad4d30749b532f1f1bee8
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-09-06 18:17:44 -0400

    Created query for inserting burgers with burger_names into our burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avacado Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
