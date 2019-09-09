83de822148bb39a01a67b954b823e5f3a0749b71
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-09-09 17:12:28 -0400

    Minor correction to update()

--- models/burger.js
+++ models/burger.js
@@ -13,8 +13,8 @@ var burger = {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update(burger_name, function(res) {
+  update: function(burger_id, cb) {
+    orm.update(burger_id, function(res) {
       cb(res);
     });
   }
