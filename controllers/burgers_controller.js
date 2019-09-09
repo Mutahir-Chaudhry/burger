26693796af529f61816203be823cf1a0a07e51ab
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-09-09 17:02:31 -0400

    Added route to devour burger. Routes should be complete at this time

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,6 +10,7 @@ router.get("/", function(req, res) {
   res.redirect("/index");
 });
 
+//Route to homepage displaying all burgers
 router.get("/index", function(req,res){
   burger.all(function(data){
     var hbsObject = { burgers: data };
@@ -17,9 +18,19 @@ router.get("/index", function(req,res){
   });
 });
 
+//Route to create a burger
+router.post("/burger/create", function(req,res){
+  burger.create(req.body.burger_name, function(){
+    res.redirect("/index");
+  });
+});
 
-
-
+//Route to devour a burger
+router.post("/burger/eat/:id", function(req,res){
+  burger.update(req.params.id, function(){
+    res.redirect("/index")
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
